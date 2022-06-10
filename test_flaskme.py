a8e55d865e4228247a32cf5db0e4f065afef15d8
Rileepher <you@example.com>
2022-06-10 10:41:37 +0800

    Pytest and DockerHub

--- test_flaskme.py
+++ test_flaskme.py
@@ -10,7 +10,8 @@ from urllib.error import HTTPError
     ]
 )
 def test_flask1(a, b, expected):
-    excepted = json.loads(urlopen(f"http://127.0.0.1:5001?a={a}&b={b}").read())
+    data = json.loads(urlopen(f"http://127.0.0.1:5001?a={a}&b={b}").read())
+    assert data == expected
 def test_flask2():
     with pytest.raises(HTTPError):
         json.loads(urlopen(f"http://127.0.0.1:5001?a=aaa&b=10").read())
